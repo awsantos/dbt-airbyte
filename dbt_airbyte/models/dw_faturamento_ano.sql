ecee5adb2b00c94bd588f7949655ff31dd6640ba
awsanto <alexandrewinkel@gmail.com>
2023-01-31 16:50:50 -0300

    First commit

---
+++ dbt_airbyte/models/dw_faturamento_ano.sql
@@ -0,0 +1,12 @@
+with dw_faturamento_ano as (
+
+select date_part('year', purchased_at ) as "Ano",
+       sum(ap.price) as "Faturamento"
+from public.airbyte_purchases pur inner join airbyte_products ap
+on pur.product_id = ap.id
+group by 1
+order by 1
+
+)
+
+select * from dw_faturamento_ano
